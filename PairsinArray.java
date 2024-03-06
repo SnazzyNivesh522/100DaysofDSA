5b49664ce6ff198fa7aef6eceae851e814155eb3
SnazzyNivesh522 <niveshpritmani@gmail.com>
2024-03-06 19:58:38 +0530

    Array First Question

--- PairsinArray.java
+++ PairsinArray.java
@@ -4,13 +4,16 @@
 public class PairsinArray {
 
     public static void  printPairs(int[] array){
+        int total=0;
         for(int i=0;i<array.length;i++){
             int curr=array[i];
             for(int j=i+1;j<array.length;j++){
                 System.out.println("("+curr+","+array[j]+")");
+                total++;
 
             }
         }
+        System.out.println(total+" pairs are present in the array.");
     }
     public static void main(String[] args) {
         int  arr[] = new int[]{2,5,9,10,11,100};
