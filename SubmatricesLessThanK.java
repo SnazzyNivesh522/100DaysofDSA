3d0fe8c4ebfc92d14f54047688cccbcc00b7061a
SnazzyNivesh522 <niveshpritmani@gmail.com>
2024-03-14 19:27:09 +0530

    added problem description as comments

--- SubmatricesLessThanK.java
+++ SubmatricesLessThanK.java
@@ -1,4 +1,14 @@
+
 // Count Submatrices with Top-Left Element and Sum Less Than k
+/*
+You are given a 0-indexed integer matrix grid and an integer k.
+
+Return the number of 
+submatrices
+ that contain the top-left element of the grid, and have a sum less than or equal to k.
+
+https://leetcode.com/problems/count-submatrices-with-top-left-element-and-sum-less-than-k/description/
+ */
 public class SubmatricesLessThanK {
     public int countSubmatrices(int[][] grid, int k) {
         int count=0;
