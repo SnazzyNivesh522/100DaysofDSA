499a76a5b183be05f656c8f1219acec8b5941ca6
SnazzyNivesh522 <niveshpritmani@gmail.com>
2024-03-24 12:05:29 +0530

    TA sessions

---
+++ MaxConsecutiveOnes.java
@@ -0,0 +1,26 @@
+public class MaxConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        //sliding window
+        int left=0;
+        int count=0;
+        int maxcount=0;
+        for(int right=0;right<nums.length;right++){
+            if(nums[right]==1){
+                count++;
+                maxcount=Math.max(maxcount,count);
+            }
+            else{
+                count=0;
+                left=right+1;
+            }
+                
+        }
+        return maxcount;        
+    }
+    public static void main(String[] args) {
+        MaxConsecutiveOnes mco = new MaxConsecutiveOnes();
+        System.out.println("Maximum consecutive 1's in array is: " + mco.findMaxConsecutiveOnes(new int[]{1,1,0,1,1,1}));
+        System.out.println("Maximum consecutive 1's in array is: " + mco.findMaxConsecutiveOnes(new int[]{1,0,1,1,0,1}));
+    }
+    
+}
