b9b27d624637b511eadcf8eaa4f73d652916c237
SnazzyNivesh522 <niveshpritmani@gmail.com>
2024-04-19 22:48:31 +0530

    binary search ques TA

---
+++ AggressiveCows.java
@@ -0,0 +1,81 @@
+/*
+You are given an array consisting of n integers which denote the position of a stall. You are also given an integer k which denotes the number of aggressive cows. You are given the task of assigning stalls to k cows such that the minimum distance between any two of them is the maximum possible.
+The first line of input contains two space-separated integers n and k.
+The second line contains n space-separated integers denoting the position of the stalls.
+
+Example 1:
+
+Input:
+n=5 
+k=3
+stalls = [1 2 4 8 9]
+Output:
+3
+Explanation:
+The first cow can be placed at stalls[0], 
+the second cow can be placed at stalls[2] and 
+the third cow can be placed at stalls[3]. 
+The minimum distance between cows, in this case, is 3, 
+which also is the largest among all possible ways.
+Example 2:
+
+Input:
+n=5 
+k=3
+stalls = [10 1 2 7 5]
+Output:
+4
+Explanation:
+The first cow can be placed at stalls[0],
+the second cow can be placed at stalls[1] and
+the third cow can be placed at stalls[4].
+The minimum distance between cows, in this case, is 4,
+which also is the largest among all possible ways.
+Your Task:
+Complete the function int solve(), which takes integer n, k, and a vector stalls with n integers as input and returns the largest possible minimum distance between cows.
+
+Expected Time Complexity: O(n*log(10^9)).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+2 <= n <= 10^5
+2 <= k <= n
+0 <= stalls[i] <= 10^9
+
+
+ */
+
+import java.util.Arrays;
+
+public class AggressiveCows {
+    private static boolean possible(int []stalls,int dist,int cows){
+        int n=stalls.length;
+        int cnt=1;
+        int first=stalls[0];
+        for(int i=1;i<n;i++){
+            if(stalls[i]-first>=dist){
+                cnt++;
+                first=stalls[i];
+            }
+            if(cnt>=cows) return true;
+        }
+        return false;
+    }
+    public static int solve(int n, int k, int[] stalls) {
+        Arrays.sort(stalls);
+        int low=1;
+        int high=stalls[n-1]-stalls[0];
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(possible(stalls,mid,k)) low=mid+1;
+            else high=mid-1;
+        }
+        return high;
+    }
+    public static void main(String[] args) {
+        System.out.println(solve(5,3,new int[]{1,2,8,4,9}));
+        System.out.println(solve(5,3,new int[]{10,1,2,7,5}));
+        System.out.println(solve(5,2,new int[]{1,2,8,4,9}));
+    }
+    
+}
