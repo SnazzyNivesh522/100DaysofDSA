fea15fe586f60b8bed5e13924bb8fe2d3ee16020
SnazzyNivesh522 <niveshpritmani@gmail.com>
2024-06-11 23:13:50 +0530

    Queue DSA session

---
+++ Queue/ReverseFirstKElementsOfQueu.java
@@ -0,0 +1,65 @@
+/*
+Given an integer K and a queue of integers, we need to reverse the order of the first K elements of the queue, leaving the other elements in the same relative order.
+
+Only following standard operations are allowed on queue.
+
+enqueue(x) : Add an item x to rear of queue
+dequeue() : Remove an item from front of queue
+size() : Returns number of elements in queue.
+front() : Finds front item.
+Note: The above operations represent the general processings. In-built functions of the respective languages can be used to solve the problem.
+
+Example 1:
+
+Input:
+5 3
+1 2 3 4 5
+Output: 
+3 2 1 4 5
+Explanation: 
+After reversing the given
+input from the 3rd position the resultant
+output will be 3 2 1 4 5.
+Example 2:
+
+Input:
+4 4
+4 3 2 1
+Output: 
+1 2 3 4
+Explanation: 
+After reversing the given
+input from the 4th position the resultant
+output will be 1 2 3 4.
+Your Task:
+Complete the provided function modifyQueue() that takes queue and K as parameters and returns a modified queue. The printing is done automatically by the driver code.
+
+Expected Time Complexity : O(N)
+Expected Auxiliary Space : O(K)
+
+Constraints:
+1 <= K <= N <= 105
+ */
+
+import java.util.Queue;
+
+public class ReverseFirstKElementsOfQueu {
+    public Queue<Integer> modifyQueue(Queue<Integer> q, int k) {
+        firstKback(q,k);
+        int remSize = q.size() - k;
+        while( remSize-- > 0){
+            int val = q.poll();
+            q.add(val);
+        }
+        return q;
+    }
+    private void firstKback(Queue<Integer> q,int k){
+        if(k == 0) return;
+        int val = q.poll();
+        firstKback(q, k - 1);
+        q.add(val);
+    }
+    public static void main(String[] args) {
+        
+    }
+}
